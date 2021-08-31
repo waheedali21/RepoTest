67560c6ec7d2117cb134a6690596d3a6ac860208
waheedali21 <waheedspc1@gmail.com>
2021-08-31 17:02:56 +0400

    new test class4

---
+++ src/TestClass4.java
@@ -0,0 +1,11 @@
+
+public class TestClass4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("After clone this new Test Class4");
+
+	}
+
+}
