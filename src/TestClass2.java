a03d856c9e9963f5822d22abf7a4490502df6fd8
waheedali21 <waheedspc1@gmail.com>
2021-08-31 16:03:46 +0400

    adding new class

---
+++ src/TestClass2.java
@@ -0,0 +1,13 @@
+
+public class TestClass2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		System.out.println("This is test class 2");
+		
+		System.out.println("Test");
+	}
+
+}
