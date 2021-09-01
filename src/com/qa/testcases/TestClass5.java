3e9d6d666c2504e113dcef27a34830de679a7b3f
waheedali21 <waheedspc1@gmail.com>
2021-09-01 10:31:00 +0400

    new package & new test class is added

---
+++ src/com/qa/testcases/TestClass5.java
@@ -0,0 +1,12 @@
+package com.qa.testcases;
+
+public class TestClass5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("this is new test class5");
+
+	}
+
+}
